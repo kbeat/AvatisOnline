7c262e186f80622cf425cf913cd378e1130c7eb5
unknown <ruslanm@PC-889.sec.local>
2012-05-24 17:53:10 +0600

    DateProvider & MainForm init

---
+++ src/kz/avatis/online/Main.java
@@ -0,0 +1,7 @@
+package kz.avatis.online;
+
+public class Main {
+	public static void main(String [] args) {
+		
+	}
+}
---
+++ src/kz/avatis/online/tools/DataProvider.java
@@ -0,0 +1,50 @@
+package kz.avatis.online.tools;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+public class DataProvider {
+	
+	private String _url;
+	private String _connectionToken;
+	Document xmlDoc;
+	
+	public DataProvider(String url) {
+		_url = url;
+		_connectionToken = null;
+	}
+	
+	private void connect(Map<String, String> parMap) {
+		
+		String query = "?";
+		
+		if (parMap != null) {
+			Set<Map.Entry<String, String>> parameters = parMap.entrySet();
+			for (Map.Entry<String, String> parameter: parameters) {
+				query += parameter.getKey() + "=" + parameter.getValue();
+			}
+		}
+		
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			xmlDoc = db.parse(new URL(_url + query).openStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public String login(Map<String, String> pars) {
+		connect(pars);
+		return null;
+	}
+
+}
